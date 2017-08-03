85edbe660542c89605d9a64b1ad2e86f03f4ec44
Alexander Shalamov <alexander_shalamov@mini-beanie.minsk.epam.com>
2017-08-03 13:51:55 +0300

    Initial extension files

---
+++ Imp/Imp Tools/SourceEditorCommand.swift
@@ -0,0 +1,46 @@
+//
+//  SourceEditorCommand.swift
+//  Imp Tools
+//
+//  Created by Alexander Shalamov on 8/3/17.
+//  Copyright © 2017 Alexander Shalamov. All rights reserved.
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorCommand: NSObject, XCSourceEditorCommand {
+    
+    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
+        
+        if invocation.buffer.lines.count == 0 {
+            completionHandler(nil)
+            return
+        }
+        
+        let text = invocation.buffer.lines
+        self.sortImports(in: text)
+        
+        completionHandler(nil)
+    }
+    
+}
+
+extension SourceEditorCommand {
+
+    fileprivate func sortImports(in text: NSMutableArray) {
+        
+        let importIndexes = text.indexesOfObjects(passingTest:) { (object, index, stop) -> Bool in
+            let line = object as! String
+            return line.contains("import")
+        }
+        
+        if importIndexes.isEmpty {
+            return
+        }
+        
+        let importLines = text.objects(at: importIndexes)
+                
+    }
+ 
+}
---
+++ Imp/Imp/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Imp
+//
+//  Created by Alexander Shalamov on 8/3/17.
+//  Copyright © 2017 Alexander Shalamov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
